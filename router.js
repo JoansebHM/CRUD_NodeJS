10dbd6aea6eed5a7eefc13edf54e52c93bd54bed
JoansebHM <joan.herrera481@pascualbravo.edu.co>
2023-09-26 13:36:30 -0500

    Upload Project

---
+++ router.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const conexion = require('./database/db')
+
+router.get('/citas', (req, res) => {
+    res.render('citas');
+})
+
+router.get('/doctores', (req, res) => {
+    res.render('doctores');
+})
+
+router.get('/', (req, res) => {
+    res.render('index');
+})
+
+router.get('/create', (req, res) => {
+    res.render('create');
+})
+
+router.get('/usuario', (req, res) => {
+    conexion.query('SELECT * FROM usuario', (err, rows) =>{
+        if (err){
+            throw err;
+        }
+        res.render('usuario', {rows: rows})
+    })
+})
+
+router.get('/edit/:IdUsuario', (req, res)=>{
+    const IdUsuario = req.params.IdUsuario;
+    conexion.query('SELECT * FROM usuario WHERE IdUsuario=?', [IdUsuario], (err, rows) =>{
+        if (err){
+            throw err;
+        }
+        res.render('edit', {rows: rows[0]})
+    })
+})
+
+router.get('/delete/:IdUsuario', (req, res)=>{
+    const IdUsuario = req.params.IdUsuario
+    conexion.query('DELETE FROM usuario WHERE IdUsuario = ?', [IdUsuario], (err)=>{
+        if (err){
+            throw err;
+        }
+        res.redirect('/usuario')
+    })
+})
+
+const crud = require('./controllers/crud')
+router.post('/save', crud.save)
+router.post('/edit', crud.update)
+
+module.exports = router;
